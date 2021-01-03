29926770848f7aae4de9db245a6f124a277be2c7
Dan <danlechambre@icloud.com>
2021-01-03 20:40:35 +0000

    Add POST method AddCharacter to CharacterController

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 
@@ -11,12 +12,25 @@ namespace Controllers
     {
         private static List<Character> characters = new List<Character> {
             new Character(),
-            new Character() {Name = "Sam"}
+            new Character() {Id = 1, Name = "Sam"}
         };
 
         [HttpGet("GetAll")]
-        public IActionResult Get()
+        public IActionResult GetCharacters()
         {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCharacter(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
             return Ok(characters);
         }
     }
